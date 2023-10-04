685ac244904717b51937585b8c9e57ecfa68b276
Camilo Meneses <kamilomeneses@gmail.com>
2023-10-03 21:10:14 -0500

    add example of nuxt application with graphql

---
+++ app.vue
@@ -0,0 +1,13 @@
+import { GqlFindAllPosts } from './.nuxt/gql/index';
+
+<script setup>
+const posts = await GqlFindAllPosts();
+/* console.log(posts); */
+</script>
+
+<template>
+  <main class="container mx-auto">
+    <UTable :rows="posts.findAllPosts" />
+  </main>
+</template>
+
